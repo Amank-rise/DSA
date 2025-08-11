6a486f2e3c2fc9756150aeb229e35e18779036ec
Amank Kumar <amank.kmr122@gmail.com>
2025-08-11 23:57:14 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (50.97%) - LeetSync

---
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+        int close = nums[0];
+        for(int num : nums){
+            if(abs(num)<abs(close)){
+                close = num;
+            }else if(abs(num)==abs(close)){
+                close = max(close,num);
+            }
+        }return close;
+    }
+};
