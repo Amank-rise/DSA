767b567023eae6a5fc1f1b02675660d10ecdc978
Amank Kumar <amank.kmr122@gmail.com>
2025-08-29 00:18:07 +0530

    Time: 51 ms (50.94%) | Memory: 55.3 MB (7.74%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-      unordered_map<long long, int> prefixSumCount;
-    prefixSumCount[0] = 1; // There's one way to have sum 0: empty subarray
-    long long sum = 0;
-    int count = 0;
-    
-    for(int num : nums) {
-        sum += num; // Current cumulative sum
+        int n = nums.size();
+       unordered_map<long long,long long>mpp;
+        long long preSum = 0,cnt = 0;
+        mpp[0]=1;
+     for (int i = 0; i < n; i++) {
+       
+        preSum += nums[i];
+
         
-        // If (current_sum - k) exists in our map, it means:
-        // There's a subarray from some previous point to current point that sums to k
-        long long needed = sum - k;
+        long long remove = preSum - k;
+
+      
+        cnt += mpp[remove];
+
         
-        if(prefixSumCount.find(needed) != prefixSumCount.end()) {
-            count += prefixSumCount[needed];
-        }
-        
-        // Record that we've seen this cumulative sum
-        prefixSumCount[sum]++;
+        mpp[preSum] += 1;
     }
-    
-    return count;
+    return cnt;
 
 }
     
